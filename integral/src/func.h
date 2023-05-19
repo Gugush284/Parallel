adc335735b2e1fef048bee33bf8487ca1060eadb
Guguch <95088312+Gugush284@users.noreply.github.com>
2023-05-19 19:25:48 +0300

    Commented func.h

--- integral/src/func.h
+++ integral/src/func.h
@@ -1,21 +1,24 @@
 #include <pthread.h>
 
-#define AMOUNT_TASKS 10000
+#define AMOUNT_TASKS 10000 // количество заданий
 
+// Данные одного задания
 typedef struct {
-    double start;
-    double end;
-    double summ;
-    double eps;
+    double start; // начало отрезка интегрирования данного задания
+    double end; // конец
+    double summ; // полученная сумма на отрезке
+    double eps; // переменная шага интегрирования
 } thread_data;
 
+// Очередь заданий
 typedef struct {
-    thread_data tasks[AMOUNT_TASKS];
-    int count;
-    pthread_mutex_t lock;
+    thread_data tasks[AMOUNT_TASKS]; // массив заданий
+    int count; // счетчик текущего задания
+    pthread_mutex_t lock; // мьютекс
 } queue;
 
+// Структура, передаваемая потоку
 typedef struct {
-    queue *q;
-    int id;
+    queue *q; // указатель на очередь
+    int id; // id данного потока
 } thread;
