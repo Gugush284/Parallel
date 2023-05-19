c11e7bcf78c1b05b0e4dd3f6523fd037f956c363
Guguch <gugush284@gmail.com>
2023-05-19 20:11:03 +0300

    Commented main

--- integral/src/main.c
+++ integral/src/main.c
@@ -3,32 +3,39 @@
 #include <pthread.h>
 #include "main.h"
 
+// объявление функций из func.c
 extern double fsin(double x);
 extern void* thr_func(void* arg);
 
+// точка входа
 int main()
 {
-    double summ;
-    pthread_t thr[num_thr];
-    int rc;
-    taskQueue queue;
-    queue.count = 0;
-    thr_struct t[num_thr];
+    double summ; // значение интеграла
+    pthread_t thr[num_thr]; // объявление массива потоков 
+    taskQueue queue; // объявление очереди задач
+    queue.count = 0; // начальное значение счетчика задач
+    thr_struct t[num_thr]; // объяление структуры для передачи в потоки
 
+    // Деление отрезка интегрирования на более мелкие, задание переменной шага интегрирования,
+    // добавление задачи в очередь задач
     for (int i = 0; i < MAX_TASKS; i++){
         queue.tasks[i].start = start + i * (end - start) / MAX_TASKS;
         queue.tasks[i].end = start + (i+1) * (end - start) / MAX_TASKS;
         queue.tasks[i].eps = eps * (i+1) * (end - start) / MAX_TASKS;
     }
 
+    // Инициализация мьютекса, блокирование потоков до тех пор, пока все не будут готовы
     pthread_mutex_init(&queue.lock, NULL);
     pthread_mutex_lock(&(queue.lock));
+
+    // Запуск потоков
     for (int i = 0; i < num_thr; i++)
     {
-        t[i].id = i;
-        t[i].queue = &queue;
-        rc = pthread_create(&thr[i], NULL, thr_func, &t[i]);
+        t[i].id = i; // присвоение номера потока
+        t[i].queue = &queue; // присвоение ссылки на очередь задач
+        int rc = pthread_create(&thr[i], NULL, thr_func, &t[i]); // создание потоков
 
+        // Проверяем, что потоки успешно созданы 
         if (rc)
         {
             fprintf(stderr, "error:pthread_create, rc:%d\n", rc);
@@ -36,14 +43,19 @@ int main()
         }
     }
 
+    printf("Threads\n");
+
+    // Разблокировка потоков для начала их работы
     pthread_mutex_unlock(&(queue.lock));
 
+    // Ожидание завершения выполнения каждого потока
     for (int i = 0; i < num_thr; i++)
         pthread_join(thr[i], NULL);
 
+    // Вычисление суммы значений, полученных в каждой выполненной задаче
     for (int i = 0; i < MAX_TASKS; i++)
         summ += queue.tasks[i].summ;
-    printf("%le\n", summ);
+    printf("integral sin(1/x) from %le to %le = %le\n", start, end, summ);
 
     return 0;
 }
--- integral/src/main.h
+++ integral/src/main.h
@@ -1,24 +1,27 @@
-#define MAX_TASKS 10000
+#define MAX_TASKS 10000 // определяет максимальное количество задач (разбиений на отрезки)
 
-const double start = 0.01;
-const double end = 3;
-const int num_thr = 8;
-const double eps = 0.0001;
+const double start = 0.01; // Начальное значение отрезка интегрирования
+const double end = 3; // Конечное значение отрезка интегрирования
+const int num_thr = 8; // Количество потоков
+const double eps = 0.000001; // Переменная шага интегрирования
 
+// Данные одного задания
 typedef struct {
-    double start;
-    double end;
-    double summ;
-    double eps;
+    double start; // начало отрезка интегрирования данного задания
+    double end; // конец
+    double summ; // полученная сумма на отрезке
+    double eps; // переменная шага интегрирования
 } task;
 
+// Очередь заданий
 typedef struct {
-    task tasks[MAX_TASKS];
-    int count;
-    pthread_mutex_t lock;
+    task tasks[MAX_TASKS]; // массив заданий
+    int count; // счетчик текущего задания
+    pthread_mutex_t lock; // мьютекс
 } taskQueue;
 
+// Структура, передаваемая потоку
 typedef struct {
-    taskQueue *queue;
-    int id;
+    taskQueue *queue; // указатель на очередь
+    int id; // id данного потока
 } thr_struct;
